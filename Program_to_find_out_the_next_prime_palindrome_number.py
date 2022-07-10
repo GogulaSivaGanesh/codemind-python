b2e90112690f9650af976708a48fdd1f604e6e6a
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-10 17:12:27 +0530

    def pal(n):     sum1=0     temp=n     while(n):         rem=n%10         sum1=sum1*10+rem         n//=10     if sum1==temp:         return 1     else:         return 0 def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c+=1     if c==2:         return 1     else:         return 0 n=int(input()) s=n+1 while(n<s):     if pal(s):         if prime(s):             print(s)             break     s+=1

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,31 @@
+def pal(n):
+    sum1=0
+    temp=n
+    while(n):
+        rem=n%10
+        sum1=sum1*10+rem
+        n//=10
+    if sum1==temp:
+        return 1
+    else:
+        return 0
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1
+    if c==2:
+        return 1
+    else:
+        return 0
+n=int(input())
+s=n+1
+while(n<s):
+    if pal(s):
+        if prime(s):
+            print(s)
+            break
+    s+=1
+        
+    
+    
