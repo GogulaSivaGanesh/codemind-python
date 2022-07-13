29d44ebe53b1ac524f04ce8d6005939069a7bab8
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-13 11:23:16 +0530

    def pal(n):     t=n     n=int(str(n)[::-1])     if t==n:         return 1     else:         return 0 n=int(input()) for i in range(n-1,0,-1):     if pal(i):         a=i         break for j in range(n+1,100000):     if pal(j):         b=j         break if abs(n-a)==abs(b-n):     print(a,b) elif abs(n-a)<abs(b-n):     print(a) else:     print(b)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,22 @@
+def pal(n):
+    t=n
+    n=int(str(n)[::-1])
+    if t==n:
+        return 1
+    else:
+        return 0
+n=int(input())
+for i in range(n-1,0,-1):
+    if pal(i):
+        a=i
+        break
+for j in range(n+1,100000):
+    if pal(j):
+        b=j
+        break
+if abs(n-a)==abs(b-n):
+    print(a,b)
+elif abs(n-a)<abs(b-n):
+    print(a)
+else:
+    print(b)
