03fef39e0d1ee5fcdca0cc3778f66a54397994c0
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-14 21:41:32 +0530

    def happy(n):     s=0     while(n):         r=n%10         s=s+r**2         n//=10     if s>9:         return happy(s)     elif s==1 or s==7:         print(True)     else:         print(False)
    n=int(input())
    happy(n)

--- happy_number.py
+++ happy_number.py
@@ -4,11 +4,13 @@ def happy(n):
         r=n%10
         s=s+r**2
         n//=10
-    return s
-a=int(input())
-while a>=10:
-    a=happy(a)
-if a==1 or a==7:
-    print(True)
-else:
-    print(False)
+    if s>9:
+        return happy(s)
+    elif s==1 or s==7:
+        print(True)
+    else:
+        print(False)
+     
+
+n=int(input())
+happy(n)
