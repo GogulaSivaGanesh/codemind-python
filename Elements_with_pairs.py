19960a19ecaa4fb3f6cb0ad2326351f2b48a1d1c
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-15 21:31:08 +0530

    a=int(input()) b=list(map(int,input().split()))
    for i in range(0,len(b)):
        print(b[i],end=' ')
    if len(b)%2==1:
        print('0')

---
+++ Elements_with_pairs.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=list(map(int,input().split()))
+
+for i in range(0,len(b)):
+    print(b[i],end=' ')
+if len(b)%2==1:
+    print('0')
+    
+            
+    
+    
+       
+        
+
+            
