65e57b191915da9f16bc03bc5350c47f4b255dcf
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-15 21:38:26 +0530

    a=int(input()) b=list(map(int,input().split())) c=[] for i in b:     if i not in c:         c.append(i)         print(i,b.count(i),end=' ')

---
+++ Element_and_Count.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=[]
+for i in b:
+    if i not in c:
+        c.append(i)
+        print(i,b.count(i),end=' ')
+
+    
+            
+    
+    
+       
+        
+
+            
