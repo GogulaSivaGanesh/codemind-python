b64726179cf9b4fae89a95188ddc8b70c336041c
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-07-19 19:44:51 +0530

    n=input() n=n.lower() n=n.split() a='aeiou' b=[] for i in n:     c=0     for j in i:         if j in a:             c+=1     b.append(c) print(min(b))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,19 @@
+n=input()
+n=n.lower()
+n=n.split()
+a='aeiou'
+b=[]
+for i in n:
+    c=0
+    for j in i:
+        if j in a:
+            c+=1
+    b.append(c)
+print(min(b))
+    
+    
+    
+    
+    
+    
+    
